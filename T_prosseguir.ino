68c05a5015adf348ea5cee623440a355b04758ea
José Léles Soares Alves <75312838+LelisSoares@users.noreply.github.com>
2021-01-03 14:24:55 -0300

    Delete T_prosseguir.ino

--- T_prosseguir.ino
+++
@@ -1,8 +0,0 @@
-void T_prosseguir(){
-  display.clearDisplay();
-  display.setCursor(0,12);
-  display.println("Prosseguir"); 
-  display.display();
-  myDFPlayer.playFolder(2,8);
-  //delay(2500);
-}
