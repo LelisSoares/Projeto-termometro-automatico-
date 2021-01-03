37045bf5d8a2cd1b7b372ee98f7b92dab469b3a4
José Léles Soares Alves <75312838+LelisSoares@users.noreply.github.com>
2021-01-03 14:24:18 -0300

    Delete T_aproximar.ino

--- T_aproximar.ino
+++
@@ -1,9 +0,0 @@
-void T_aproximar(){
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(0,12);
-  display.println(" Aproximar"); 
-  display.display();
-  myDFPlayer.playFolder(2,2);
-  delay(3500);
-}
